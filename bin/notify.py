aba4aed877156d5f4a507e64320c9badbea0c160
Peter Nguyen <peter@datacrunch.ca>
2021-12-15 19:50:34 -0500

    Updated revision to show heroku release version instead

--- bin/notify.py
+++ bin/notify.py
@@ -3,18 +3,18 @@ import requests
 import subprocess
 import traceback
 
-
 env_dir = os.environ['ENV_DIR']
 new_relic_api_key = os.popen('cat {}/NEW_RELIC_API_KEY'.format(env_dir)).read()
 new_relic_app_id = os.popen('cat {}/NEW_RELIC_APP_ID'.format(env_dir)).read()
 deployer = os.popen('cat {}/DEPLOYER'.format(env_dir)).read()
 commit_id = os.environ['SOURCE_VERSION']
+release_version = os.environ['HEROKU_RELEASE_VERSION']
 
 url = 'https://api.newrelic.com/v2/applications/{}/deployments.json'.format(new_relic_app_id)
 
 deployment_info = {
     "deployment": {
-        "revision": commit_id,
+        "revision": release_version,
         "changelog": "Deploying {}".format(commit_id),
         "description": "Deploying {}".format(commit_id),
         "user": deployer
